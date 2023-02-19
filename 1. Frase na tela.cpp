20bbf96f719f3c8843c272f42d31e6bef34185ad
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 14:58:45 -0300

    Update 1. Frase na tela.cpp

--- 1. Frase na tela.cpp
+++ 1. Frase na tela.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+//Implemente um programa que escreve na tela a frase "O primeiro programa a
 
+gente nunca esquece!".
 int main(void){
 	
 	printf("O primeiro programa a gente nunca esquece!");
