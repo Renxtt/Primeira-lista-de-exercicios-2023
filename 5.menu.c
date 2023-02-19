1f5f4ae128565750db0f00a47ffe7753d4fe562c
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 15:02:39 -0300

    Update 5.menu.c

--- 5.menu.c
+++ 5.menu.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+//Elabore um programa que mostre o seguinte menu na tela:
 
+Cadastro de Clientes
 main(void){
-	printf("\nCadastro de Clientes\n\n0 - Fim\n	\n1 - Inclui\n\n2 - Altera\n\n3 - Exclui\n\n4 - Consulta\n\nOpção:\n");
+	printf("\nCadastro de Clientes\n\n0 -Â Fim\n	\n1 -Â Inclui\n\n2 -Â Altera\n\n3 -Â Exclui\n\n4 -Â Consulta\n\nOpÃ§Ã£o:\n");
 	
 	
 }
