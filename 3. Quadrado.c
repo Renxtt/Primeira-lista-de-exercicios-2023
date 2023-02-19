70008473050da6eff4893def847a8bf928f9c1c9
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 15:01:31 -0300

    Update 3. Quadrado.c

--- 3. Quadrado.c
+++ 3. Quadrado.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+/*Escrever um programa que mostre a seguinte figura no alto da tela:
 
+`XXXXX
+X   X
+X   X
+X   X
+XXXXX`*/
 int main(void){
 	printf("\nxxxxx\nx   x\nx   x\nx   x\nxxxxx");
 	return 0;
