ad3f28b03e7d3aaffa090e63bc8e0b7fdfd3a2f2
Renxtt <122226822+Renxtt@users.noreply.github.com>
2023-02-19 16:36:39 -0300

    Add files via upload

---
+++ 000.exerciciosdaprimeiralista.cpp
@@ -0,0 +1,68 @@
+/*
+
+
+	# Exercícios 1
+	
+	**1. Frase na tela**  - Implemente um programa que escreve na tela a frase "O primeiro programa a
+	
+	gente nunca esquece!".
+	
+	**2. Etiqueta** - Elabore um programa que pede seu nome, endereço, CEP e telefone. Ele deve imprirmir seu nome completo na primeira linha, seu endereço na segunda, e o CEP e telefone na terceira.
+	
+	3**. Quadrado** - Escrever um programa que mostre a seguinte figura no alto da tela:
+	
+	`XXXXX
+	X   X
+	X   X
+	X   X
+	XXXXX`
+	
+	4**. Tabela de notas** - Escreva um programa que produza a seguinte saída na tela:
+	
+	`ALUNO(A)          NOTA
+	=========         =====
+	ALINE              9.0  
+	MÁRIO              DEZ
+	SÉRGIO             4.5    
+	SHIRLEY            7.0`
+	
+	5**. Menu** - Elabore um programa que mostre o seguinte menu na tela:
+	
+	Cadastro de Clientes
+	
+	0 - Fim
+	
+	1 - Inclui
+	
+	2 - Altera
+	
+	3 - Exclui
+	
+	4 - Consulta
+	
+	Opção:
+	
+	6**. Pinheiro**  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+	
+	Enriqueça o desenho com outros caracteres, simulando enfeites.
+	
+	       `X
+	      XXX
+	     XXXXX
+	    XXXXXXX
+	   XXXXXXXXX
+	  XXXXXXXXXXX
+	 XXXXXXXXXXXXX
+	XXXXXXXXXXXXXXX
+	       XX
+	       XX
+	      XXXX`
+	
+	1. Escreva um programa que leia o nome de uma pessoa e exiba uma saudação na tela.
+	2. Escreva um programa que leia dois números inteiros e exiba a soma desses números na tela.
+	3. Escreva um programa que leia o raio de um círculo e calcule sua área e seu perímetro.
+	4. Escreva um programa que leia dois números inteiros e exiba o resultado da divisão inteira desses números.
+	5. Escreva um programa que leia o nome, o salário e o ano de nascimento de uma pessoa e exiba essas informações na tela.
+	6. Escreva um programa que leia o preço de um produto e a quantidade comprada e exiba o valor total da compra.
+	
+	*/
